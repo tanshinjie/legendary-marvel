6b8035076a1da274858f63a839233fccd9b8b70d
Tan Shin Jie <shinjie.tan@alta.exchange>
2023-07-01 18:53:23 +0800

    Completed basic setup

--- scripts/parse.js
+++ scripts/parse.js
@@ -10,60 +10,83 @@ const schemes = [];
 const freeVillains = [];
 const freeHenchmen = [];
 
-fs.createReadStream(path.resolve(__dirname, "../data/_raw.csv"))
+// fs.createReadStream(path.resolve(__dirname, "../data/_raw.csv"))
+//   .pipe(csv())
+//   .on("data", (data) => results.push(data))
+//   .on("end", () => {
+//     results.forEach((result, idx) => {
+//       if (result["Heroes"]) {
+//         heros.push({
+//           id: idx + 1,
+//           name: result["Heroes"],
+//         });
+//       }
+//       if (result["Mastermind"]) {
+//         masterminds.push({
+//           id: idx + 1,
+//           name: result["Mastermind"],
+//         });
+//         if (result["Scheme"]) {
+//           schemes.push({
+//             id: idx + 1,
+//             name: result["Scheme"],
+//           });
+//         }
+//         if (result["Free Henchman"]) {
+//           freeHenchmen.push({
+//             id: idx + 1,
+//             name: result["Free Henchman"],
+//           });
+//         }
+//         if (result["Free Villains"]) {
+//           freeVillains.push({
+//             id: idx + 1,
+//             name: result["Free Villains"],
+//           });
+//         }
+//       }
+//     });
+//     fs.writeFileSync(
+//       path.resolve(__dirname, "../public/heroes.json"),
+//       JSON.stringify(heros)
+//     );
+//     fs.writeFileSync(
+//       path.resolve(__dirname, "../public/masterminds.json"),
+//       JSON.stringify(masterminds)
+//     );
+//     fs.writeFileSync(
+//       path.resolve(__dirname, "../public/schemes.json"),
+//       JSON.stringify(schemes)
+//     );
+//     fs.writeFileSync(
+//       path.resolve(__dirname, "../public/freeVillains.json"),
+//       JSON.stringify(freeVillains)
+//     );
+//     fs.writeFileSync(
+//       path.resolve(__dirname, "../public/freeHenchmen.json"),
+//       JSON.stringify(freeHenchmen)
+//     );
+//   });
+
+const _mastermind = [];
+const mastermindAlwaysLead = [];
+fs.createReadStream(path.resolve(__dirname, "../data/_mastermind.csv"))
   .pipe(csv())
-  .on("data", (data) => results.push(data))
+  .on("data", (data) => _mastermind.push(data))
   .on("end", () => {
-    results.forEach((result, idx) => {
-      if (result["Heroes"]) {
-        heros.push({
-          id: idx + 1,
-          name: result["Heroes"],
-        });
-      }
+    _mastermind.forEach((result, idx) => {
       if (result["Mastermind"]) {
-        masterminds.push({
+        let mastermind = result["Mastermind"];
+        let alwaysLead = result["Always Lead"];
+        mastermindAlwaysLead.push({
           id: idx + 1,
-          name: result["Mastermind"],
+          mastermind,
+          alwaysLead,
         });
-        if (result["Scheme"]) {
-          schemes.push({
-            id: idx + 1,
-            name: result["Scheme"],
-          });
-        }
-        if (result["Free Henchman"]) {
-          freeHenchmen.push({
-            id: idx + 1,
-            name: result["Free Henchman"],
-          });
-        }
-        if (result["Free Villains"]) {
-          freeVillains.push({
-            id: idx + 1,
-            name: result["Free Villains"],
-          });
-        }
       }
+      fs.writeFileSync(
+        path.resolve(__dirname, "../public/mastermindAlwaysLead.json"),
+        JSON.stringify(mastermindAlwaysLead)
+      );
     });
-    fs.writeFileSync(
-      path.resolve(__dirname, "../public/heroes.json"),
-      JSON.stringify(heros)
-    );
-    fs.writeFileSync(
-      path.resolve(__dirname, "../public/masterminds.json"),
-      JSON.stringify(masterminds)
-    );
-    fs.writeFileSync(
-      path.resolve(__dirname, "../public/schemes.json"),
-      JSON.stringify(schemes)
-    );
-    fs.writeFileSync(
-      path.resolve(__dirname, "../public/freeVillains.json"),
-      JSON.stringify(freeVillains)
-    );
-    fs.writeFileSync(
-      path.resolve(__dirname, "../public/freeHenchmen.json"),
-      JSON.stringify(freeHenchmen)
-    );
   });
--- src/app/page.js
+++ src/app/page.js
@@ -6,14 +6,25 @@ import masterminds from "../../public/masterminds.json";
 import schemes from "../../public/schemes.json";
 import freeVillains from "../../public/freeVillains.json";
 import freeHenchmen from "../../public/freeHenchmen.json";
+import mastermindAlwaysLead from "../../public/mastermindAlwaysLead.json";
 
 export default function Home() {
-  const [campaignState, setCampaignState] = useState(randomCampaign());
+  const [gameState, setGameState] = useState(null);
+  const [campaignState, setCampaignState] = useState({
+    activeConsequences: [],
+    freeAgents: [],
+  });
+  const [gameLog, setGameLog] = useState([]);
   const [steps, setSteps] = useState(0);
   const [numOfAgency, setNumOfAgency] = useState(1);
   const [agencyHeros, setAgencyHeros] = useState({});
   const [freeVillains, setFreeVillains] = useState([]);
   const [freeHenchmen, setFreeHenchmen] = useState([]);
+  const [gameResult, setGameResult] = useState();
+
+  const generateRandomGame = () => {
+    setGameState(randomCampaign());
+  };
 
   const handleAgencyInput = (e) => {
     setNumOfAgency(e.target.value);
@@ -25,43 +36,71 @@ export default function Home() {
     setAgencyHeros(agencyHeros);
   };
 
-  const generateHeroesForAgency = (agencyName) => () => {
-    const randomHero1 = heroes[Math.floor(Math.random() * heroes.length)];
-    const randomHero2 = heroes[Math.floor(Math.random() * heroes.length)];
-    const randomHero3 = heroes[Math.floor(Math.random() * heroes.length)];
-    const newAgencyHeroes = {
-      ...agencyHeros,
-      [agencyName]: [randomHero1, randomHero2, randomHero3],
-    };
-    setAgencyHeros(newAgencyHeroes);
+  const startGame = () => {
+    const newGameState = { ...gameState };
+    newGameState.startTime = new Date();
+    newGameState.freeVillains = freeVillains;
+    newGameState.freeHenchmen = freeHenchmen;
+    setGameState(newGameState);
+    setSteps(5);
+  };
+
+  const endGame = () => {
+    const newGameState = { ...gameState };
+    newGameState.endTime = new Date();
+    setGameState(newGameState);
+    setSteps(6);
+  };
+
+  const saveGame = () => {
+    const newGameState = { ...gameState };
+    newGameState.gameResult = gameResult;
+    setGameState(newGameState);
+    setGameLog([...gameLog, newGameState]);
+    setSteps(7);
+  };
+
+  const nextGame = () => {
+    console.log("nextGame");
   };
 
   return (
     <main>
       {steps >= 0 && (
         <>
-          <h1 className="text-2xl font-bold">Marvel Legendary Randomizer</h1>
+          <h1 className="text-2xl font-bold">
+            Marvel Legendary Campaign Timeline
+          </h1>
           <br />
           <h1 className="font-bold">Main Hero</h1>
-          <h2>{campaignState.hero.name}</h2>
+          <h2>{gameState && gameState.hero.name}</h2>
           <br />
           <h1 className="font-bold">Mastermind</h1>
-          <h2>{campaignState.mastermind.name}</h2>
+          <h2>{gameState && gameState.mastermind.name}</h2>
+          <h1 className="font-bold">Always lead</h1>
+          <h2>
+            {gameState &&
+              JSON.stringify(
+                mastermindAlwaysLead.filter(
+                  (item) => item.mastermind === gameState.mastermind.name
+                )
+              )}
+          </h2>
           <br />
           <h1 className="font-bold">Scheme</h1>
-          <h2>{campaignState.scheme.name}</h2>
+          <h2>{gameState && gameState.scheme.name}</h2>
           <div className="space-x-2">
+            <button
+              className="bg-blue-500 py-2 px-4"
+              onClick={generateRandomGame}
+            >
+              Generate
+            </button>
             <button
               className="bg-blue-500 py-2 px-4"
               onClick={() => setSteps(1)}
             >
               Next
-            </button>
-            <button
-              className="bg-blue-500 py-2 px-4"
-              onClick={() => setCampaignState(randomCampaign())}
-            >
-              Refresh
             </button>
           </div>
         </>
@@ -85,12 +124,28 @@ export default function Home() {
               <>
                 <div key={key}>Agency Name: {key}</div>
                 <div>Heroes: {JSON.stringify(agencyHeros[key])}</div>
-                <button
-                  className="bg-blue-500 py-2 px-4"
-                  onClick={generateHeroesForAgency(key)}
+                <select
+                  name="hero"
+                  id="hero"
+                  multiple={true}
+                  onChange={(e) => {
+                    const options = [...e.target.selectedOptions];
+                    const values = options.map((option) => option.value);
+                    const newAgencyHeroes = {
+                      ...agencyHeros,
+                      [key]: [...values],
+                    };
+                    setAgencyHeros(newAgencyHeroes);
+                  }}
                 >
-                  Click to generate randomized hero
-                </button>
+                  {heroes.map((hero) => {
+                    return (
+                      <option key={hero.id} value={hero.name}>
+                        {hero.name}
+                      </option>
+                    );
+                  })}
+                </select>
                 <div className="mt-4" />
               </>
             );
@@ -134,11 +189,58 @@ export default function Home() {
             >
               Click to generate randomized free henchmen
             </button>
-            <button
-              className="bg-blue-500 py-2 px-4"
-              onClick={() => setSteps(5)}
-            >
-              Next
+            <button className="bg-blue-500 py-2 px-4" onClick={startGame}>
+              Start game
+            </button>
+          </div>
+        </>
+      )}
+      <div className="mt-4" />
+      {steps >= 5 && (
+        <>
+          <h1 className="font-bold">
+            Start time: {gameState.startTime.toLocaleString()}
+          </h1>
+          <div className="space-x-2">
+            <button className="bg-blue-500 py-2 px-4" onClick={endGame}>
+              End game
+            </button>
+          </div>
+        </>
+      )}
+      <div className="mt-4" />
+      {steps >= 6 && (
+        <>
+          <h1 className="font-bold">
+            End time: {gameState.endTime.toLocaleString()}
+          </h1>
+          <label htmlFor="gameResult">Game result</label>
+          <select
+            name="gameResult"
+            id="gameResult"
+            onChange={(e) => {
+              setGameResult(e.target.value);
+            }}
+          >
+            <option value="win">Win</option>
+            <option value="lose">Lose</option>
+            <option value="draw">Draw</option>
+          </select>
+          <div className="space-x-2">
+            <button className="bg-blue-500 py-2 px-4" onClick={saveGame}>
+              Save
+            </button>
+          </div>
+        </>
+      )}
+      <div className="mt-4" />
+      {steps >= 7 && (
+        <>
+          <h1 className="font-bold">Game summary:</h1>
+          <div>{JSON.stringify(gameState)}</div>
+          <div className="space-x-2">
+            <button className="bg-blue-500 py-2 px-4" onClick={nextGame}>
+              Next game
             </button>
           </div>
         </>
@@ -154,7 +256,6 @@ function randomCampaign() {
     scheme: randomScheme(),
   };
 }
-
 function randomHero() {
   return heroes[Math.floor(Math.random() * heroes.length)];
 }
