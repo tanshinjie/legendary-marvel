70c7c0dde634b25f1bac845e037b2027dfdc78e7
Tan Shin Jie <shinjie.tan@alta.exchange>
2023-07-15 10:58:57 +0800

    Added combobox for henchmen and villain

--- src/app/game/new/page.js
+++ src/app/game/new/page.js
@@ -1,17 +1,19 @@
 "use client";
 
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useState, Fragment } from "react";
 import consequences from "../../../../public/consequences.json";
 import heroes from "../../../../public/heroes.json";
 import masterminds from "../../../../public/masterminds.json";
 import schemes from "../../../../public/schemes.json";
-import freeVillains from "../../../../public/freeVillains.json";
-import freeHenchmen from "../../../../public/freeHenchmen.json";
+import freeVillainsRaw from "../../../../public/freeVillains.json";
+import freeHenchmenRaw from "../../../../public/freeHenchmen.json";
 import mastermindAlwaysLead from "../../../../public/mastermindAlwaysLead.json";
 import { useRouter, useSearchParams } from "next/navigation";
 import { fetcher } from "../../../core/fetcher";
 import useSWR from "swr";
 import { CampaignContext } from "../../../context/campaign";
+import { Combobox, Transition } from "@headlessui/react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 
 export default function Game() {
   const [gameState, setGameState] = useState(null);
@@ -258,12 +260,11 @@ export default function Game() {
           <h1 className="font-bold">Free villains</h1>
           <div>{JSON.stringify(freeVillains)}</div>
           <div className="space-x-2">
-            <button
-              className="bg-blue-500 py-2 px-4"
-              onClick={() => setFreeVillains([randomFreeVillains()])}
-            >
-              Click to generate randomized free villains
-            </button>
+            <CustomCombobox
+              options={freeVillainsRaw}
+              selected={freeVillains}
+              onChangeHandler={setFreeVillains}
+            />
             <button
               className="bg-blue-500 py-2 px-4"
               onClick={() => setSteps(3)}
@@ -279,12 +280,11 @@ export default function Game() {
           <h1 className="font-bold">Free henchmen</h1>
           <div>{JSON.stringify(freeHenchmen)}</div>
           <div className="space-x-2">
-            <button
-              className="bg-blue-500 py-2 px-4"
-              onClick={() => setFreeHenchmen([randomHenchmen()])}
-            >
-              Click to generate randomized free henchmen
-            </button>
+            <CustomCombobox
+              options={freeHenchmenRaw}
+              selected={freeHenchmen}
+              onChangeHandler={setFreeHenchmen}
+            />
             <button
               className="bg-blue-500 py-2 px-4"
               onClick={() => setSteps(4)}
@@ -390,4 +390,87 @@ function randomFreeVillains() {
 }
 function randomHenchmen() {
   return freeHenchmen[Math.floor(Math.random() * freeHenchmen.length)];
+}
+
+function CustomCombobox(props) {
+  const { options, onChangeHandler, selected } = props;
+  const [query, setQuery] = useState("");
+
+  const filteredPeople =
+    query === ""
+      ? options
+      : options.filter((person) =>
+          person.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  return (
+    <Combobox value={selected} onChange={onChangeHandler}>
+      <div className="relative mt-1">
+        <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+          <Combobox.Input
+            className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+            displayValue={(person) => person.name}
+            onChange={(event) => setQuery(event.target.value)}
+          />
+          <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+            <ChevronUpDownIcon
+              className="h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+          </Combobox.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          leave="transition ease-in duration-100"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+          afterLeave={() => setQuery("")}
+        >
+          <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            {filteredPeople.length === 0 && query !== "" ? (
+              <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                Nothing found.
+              </div>
+            ) : (
+              filteredPeople.map((person) => (
+                <Combobox.Option
+                  key={person.id}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                      active ? "bg-teal-600 text-white" : "text-gray-900"
+                    }`
+                  }
+                  value={person}
+                >
+                  {({ selected, active }) => (
+                    <>
+                      <span
+                        className={`block truncate ${
+                          selected ? "font-medium" : "font-normal"
+                        }`}
+                      >
+                        {person.name}
+                      </span>
+                      {selected ? (
+                        <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? "text-white" : "text-teal-600"
+                          }`}
+                        >
+                          <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                        </span>
+                      ) : null}
+                    </>
+                  )}
+                </Combobox.Option>
+              ))
+            )}
+          </Combobox.Options>
+        </Transition>
+      </div>
+    </Combobox>
+  );
 }
